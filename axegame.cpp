a091e0da145e7076b9a81ee35afa52b3c8670c3a
ansakan06 <ansakanwal2145@gmail.com>
2025-03-19 23:28:43 -0400

    Added Circle and the ability to move left and right

--- axegame.cpp
+++ axegame.cpp
@@ -2,5 +2,34 @@
 
 int main()
 {
-    InitWindow(1350,1200,"Ansa");
+    int width{450};
+    int height{350};
+    InitWindow(width,height,"Axe Game");
+
+    int circlex{225};
+    int circley{175};
+
+    SetTargetFPS(60); //sets time for how fast it upgrades
+    while (WindowShouldClose() == false)
+    {
+        BeginDrawing();
+        ClearBackground(PINK);
+
+
+        DrawCircle(circlex,circley,25, BLUE);
+
+        if (IsKeyDown(KEY_RIGHT)&& circlex<450)
+        {
+            circlex=circlex+5;
+        }
+
+        if (IsKeyDown(KEY_LEFT)&& circlex>0)
+        {
+            circlex=circlex-5;
+        }
+
+        EndDrawing();
+    }
+
+
 }
