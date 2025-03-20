cfd1be73d63aee6f715145367f89e3e6414d7a75
ansakan06 <ansakanwal2145@gmail.com>
2025-03-20 14:52:56 -0400

    finished

--- axegame.cpp
+++ axegame.cpp
@@ -19,13 +19,20 @@ int main()
     int axex{400};
     int axey{0};
     int axelength{50};
-//axe adges
+//axe edges
     int l_axex{axex};
     int r_axex{axex+axelength};
     int u_axey{axey};
     int b_axey{axey+axelength};
 
-    int direction{5};
+    int directionY{10};
+    int directionX{7}
+
+    bool collisionWithAxe = 
+                            (b_axey>=u_circley) &&
+                            (u_axey<=b_circley) && 
+                            (r_axex >= l_circlex) && 
+                            (l_axex<=r_circlex);
 
     SetTargetFPS(60); //sets time for how fast it upgrades
     while (WindowShouldClose() == false)
@@ -33,26 +40,57 @@ int main()
         BeginDrawing();
         ClearBackground(PINK);
 
-
-        DrawCircle(circlex,circley,circleRadius, BLUE);
-        DrawRectangle(axex,axey,axelength,axelength,WHITE);
-
-        axey+=direction;
-        if(axey>height|| axey<0)
+        if (collisionWithAxe)
         {
-            direction = -direction;
+            DrawText("GAME OVER!",400,200,50,RED);
+        }
+        else
+        {
+            //update the edges
+            l_circlex=circlex-circleRadius;
+            r_circlex=circlex+circleRadius;
+            u_circley=circley-circleRadius;
+            b_circley=circley-circleRadius;
 
+            l_axex=axex;
+            r_axex=axex +axelength;
+            u_axey=axey;
+            b_axey=axey+axelength;
+
+            collisionWithAxe = 
+                            (b_axey>=u_circley) &&
+                            (u_axey<=b_circley) && 
+                            (r_axex >= l_circlex) && 
+                            (l_axex<=r_circlex);
+
+            DrawCircle(circlex,circley,circleRadius, BLUE);
+            DrawRectangle(axex,axey,axelength,axelength,WHITE);
+    
+            axey += directionY;
+            if (axey > height - axelength || axey < 0)
+            {
+                directionY = -directionY;  
+            }
+
+            axex += directionX;
+            if (axex > width - axelength || axex < 0)
+            {
+                directionX = -directionX;  
+
+    
+            if (IsKeyDown(KEY_RIGHT)&& circlex<width)
+            {
+                circlex=circlex+5;
+            }
+    
+            if (IsKeyDown(KEY_LEFT)&& circlex>0)
+            {
+                circlex=circlex-5;
+            }
         }
 
-        if (IsKeyDown(KEY_RIGHT)&& circlex<width)
-        {
-            circlex=circlex+5;
-        }
 
-        if (IsKeyDown(KEY_LEFT)&& circlex>0)
-        {
-            circlex=circlex-5;
-        }
+
 
         EndDrawing();
     }
